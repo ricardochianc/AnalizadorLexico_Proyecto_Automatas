fa08508a4579a006bc20d093b00c434c0320efd3
Ricardo Chian <ricardochianc@gmail.com>
2020-05-24 18:37:27 -0600

    Se arregló problema al leer dobles rangos en los tokens

--- ProyectoFinal_RicardoChian/ProyectoFinal_RicardoChian/Home.Designer.cs
+++ ProyectoFinal_RicardoChian/ProyectoFinal_RicardoChian/Home.Designer.cs
@@ -35,31 +35,31 @@
             this.btn_CargarArchivo = new System.Windows.Forms.PictureBox();
             this.openFileDialog_archivo = new System.Windows.Forms.OpenFileDialog();
             this.gbx_ResultadoAnalisis = new System.Windows.Forms.GroupBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.dgv_Sets = new System.Windows.Forms.DataGridView();
-            this.dgtv_Tokens = new System.Windows.Forms.DataGridView();
-            this.lb_ExpresionRegular = new System.Windows.Forms.Label();
             this.btn_continuarAutomata = new System.Windows.Forms.PictureBox();
+            this.lb_ExpresionRegular = new System.Windows.Forms.Label();
+            this.dgtv_Tokens = new System.Windows.Forms.DataGridView();
+            this.dgv_Sets = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.gbx_Automata = new System.Windows.Forms.GroupBox();
+            this.dgtv_Transiciones = new System.Windows.Forms.DataGridView();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dgv_Follows = new System.Windows.Forms.DataGridView();
+            this.label6 = new System.Windows.Forms.Label();
             this.btn_GenerarArchivo = new System.Windows.Forms.PictureBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.dgv_Follows = new System.Windows.Forms.DataGridView();
-            this.label7 = new System.Windows.Forms.Label();
-            this.dgtv_Transiciones = new System.Windows.Forms.DataGridView();
             this.gbx_CargarArchivo.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.btn_continuar)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.btn_CargarArchivo)).BeginInit();
             this.gbx_ResultadoAnalisis.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dgv_Sets)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dgtv_Tokens)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.btn_continuarAutomata)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgtv_Tokens)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Sets)).BeginInit();
             this.gbx_Automata.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.btn_GenerarArchivo)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dgv_Follows)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgtv_Transiciones)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Follows)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_GenerarArchivo)).BeginInit();
             this.SuspendLayout();
             // 
             // gbx_CargarArchivo
@@ -71,7 +71,7 @@
             this.gbx_CargarArchivo.Controls.Add(this.btn_CargarArchivo);
             this.gbx_CargarArchivo.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.gbx_CargarArchivo.ForeColor = System.Drawing.Color.White;
-            this.gbx_CargarArchivo.Location = new System.Drawing.Point(12, 12);
+            this.gbx_CargarArchivo.Location = new System.Drawing.Point(11, 12);
             this.gbx_CargarArchivo.Name = "gbx_CargarArchivo";
             this.gbx_CargarArchivo.Size = new System.Drawing.Size(670, 430);
             this.gbx_CargarArchivo.TabIndex = 0;
@@ -142,32 +142,47 @@
             this.gbx_ResultadoAnalisis.Controls.Add(this.label3);
             this.gbx_ResultadoAnalisis.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.gbx_ResultadoAnalisis.ForeColor = System.Drawing.Color.White;
-            this.gbx_ResultadoAnalisis.Location = new System.Drawing.Point(12, 12);
+            this.gbx_ResultadoAnalisis.Location = new System.Drawing.Point(9, 9);
             this.gbx_ResultadoAnalisis.Name = "gbx_ResultadoAnalisis";
             this.gbx_ResultadoAnalisis.Size = new System.Drawing.Size(670, 430);
             this.gbx_ResultadoAnalisis.TabIndex = 4;
             this.gbx_ResultadoAnalisis.TabStop = false;
             this.gbx_ResultadoAnalisis.Text = "2. Resultado de análisis de archivo";
             // 
-            // label3
+            // btn_continuarAutomata
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(170, 47);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(48, 20);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "SETS:";
+            this.btn_continuarAutomata.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_continuarAutomata.Image = global::ProyectoFinal_RicardoChian.Properties.Resources.ContinuarVerde;
+            this.btn_continuarAutomata.Location = new System.Drawing.Point(580, 372);
+            this.btn_continuarAutomata.Name = "btn_continuarAutomata";
+            this.btn_continuarAutomata.Size = new System.Drawing.Size(70, 37);
+            this.btn_continuarAutomata.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btn_continuarAutomata.TabIndex = 7;
+            this.btn_continuarAutomata.TabStop = false;
+            this.btn_continuarAutomata.Click += new System.EventHandler(this.btn_continuarAutomata_Click);
             // 
-            // label2
+            // lb_ExpresionRegular
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(345, 47);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(73, 20);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "TOKENS:";
+            this.lb_ExpresionRegular.AutoSize = true;
+            this.lb_ExpresionRegular.Location = new System.Drawing.Point(66, 354);
+            this.lb_ExpresionRegular.Name = "lb_ExpresionRegular";
+            this.lb_ExpresionRegular.Size = new System.Drawing.Size(0, 17);
+            this.lb_ExpresionRegular.TabIndex = 6;
+            // 
+            // dgtv_Tokens
+            // 
+            this.dgtv_Tokens.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dgtv_Tokens.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgtv_Tokens.ColumnHeadersVisible = false;
+            this.dgtv_Tokens.Cursor = System.Windows.Forms.Cursors.No;
+            this.dgtv_Tokens.GridColor = System.Drawing.SystemColors.ButtonFace;
+            this.dgtv_Tokens.Location = new System.Drawing.Point(349, 80);
+            this.dgtv_Tokens.Name = "dgtv_Tokens";
+            this.dgtv_Tokens.ReadOnly = true;
+            this.dgtv_Tokens.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dgtv_Tokens.RowHeadersVisible = false;
+            this.dgtv_Tokens.Size = new System.Drawing.Size(96, 209);
+            this.dgtv_Tokens.TabIndex = 5;
             // 
             // dgv_Sets
             // 
@@ -186,40 +201,35 @@
             this.dgv_Sets.Size = new System.Drawing.Size(103, 209);
             this.dgv_Sets.TabIndex = 4;
             // 
-            // dgtv_Tokens
+            // label4
             // 
-            this.dgtv_Tokens.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
-            this.dgtv_Tokens.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgtv_Tokens.ColumnHeadersVisible = false;
-            this.dgtv_Tokens.Cursor = System.Windows.Forms.Cursors.No;
-            this.dgtv_Tokens.GridColor = System.Drawing.SystemColors.ButtonFace;
-            this.dgtv_Tokens.Location = new System.Drawing.Point(349, 80);
-            this.dgtv_Tokens.Name = "dgtv_Tokens";
-            this.dgtv_Tokens.ReadOnly = true;
-            this.dgtv_Tokens.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
-            this.dgtv_Tokens.RowHeadersVisible = false;
-            this.dgtv_Tokens.Size = new System.Drawing.Size(96, 209);
-            this.dgtv_Tokens.TabIndex = 5;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(34, 322);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(168, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "EXPRESIÓN REGULAR:";
             // 
-            // lb_ExpresionRegular
+            // label2
             // 
-            this.lb_ExpresionRegular.AutoSize = true;
-            this.lb_ExpresionRegular.Location = new System.Drawing.Point(66, 354);
-            this.lb_ExpresionRegular.Name = "lb_ExpresionRegular";
-            this.lb_ExpresionRegular.Size = new System.Drawing.Size(0, 17);
-            this.lb_ExpresionRegular.TabIndex = 6;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(345, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "TOKENS:";
             // 
-            // btn_continuarAutomata
+            // label3
             // 
-            this.btn_continuarAutomata.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btn_continuarAutomata.Image = global::ProyectoFinal_RicardoChian.Properties.Resources.ContinuarVerde;
-            this.btn_continuarAutomata.Location = new System.Drawing.Point(580, 372);
-            this.btn_continuarAutomata.Name = "btn_continuarAutomata";
-            this.btn_continuarAutomata.Size = new System.Drawing.Size(70, 37);
-            this.btn_continuarAutomata.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.btn_continuarAutomata.TabIndex = 7;
-            this.btn_continuarAutomata.TabStop = false;
-            this.btn_continuarAutomata.Click += new System.EventHandler(this.btn_continuarAutomata_Click);
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(170, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "SETS:";
             // 
             // gbx_Automata
             // 
@@ -239,54 +249,13 @@
             this.gbx_Automata.TabStop = false;
             this.gbx_Automata.Text = "3. Autómata";
             // 
-            // btn_GenerarArchivo
+            // dgtv_Transiciones
             // 
-            this.btn_GenerarArchivo.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btn_GenerarArchivo.Image = global::ProyectoFinal_RicardoChian.Properties.Resources.ContinuarVerde;
-            this.btn_GenerarArchivo.Location = new System.Drawing.Point(587, 371);
-            this.btn_GenerarArchivo.Name = "btn_GenerarArchivo";
-            this.btn_GenerarArchivo.Size = new System.Drawing.Size(70, 37);
-            this.btn_GenerarArchivo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.btn_GenerarArchivo.TabIndex = 7;
-            this.btn_GenerarArchivo.TabStop = false;
-            this.btn_GenerarArchivo.Click += new System.EventHandler(this.btn_GenerarArchivo_Click);
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(66, 354);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(0, 17);
-            this.label5.TabIndex = 6;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(34, 322);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(168, 20);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "EXPRESIÓN REGULAR:";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft YaHei UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(15, 35);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(124, 20);
-            this.label6.TabIndex = 8;
-            this.label6.Text = "Tabla de Follows:";
-            // 
-            // dgv_Follows
-            // 
-            this.dgv_Follows.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv_Follows.Location = new System.Drawing.Point(19, 58);
-            this.dgv_Follows.Name = "dgv_Follows";
-            this.dgv_Follows.RowHeadersVisible = false;
-            this.dgv_Follows.Size = new System.Drawing.Size(219, 150);
-            this.dgv_Follows.TabIndex = 9;
+            this.dgtv_Transiciones.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgtv_Transiciones.Location = new System.Drawing.Point(19, 248);
+            this.dgtv_Transiciones.Name = "dgtv_Transiciones";
+            this.dgtv_Transiciones.Size = new System.Drawing.Size(556, 150);
+            this.dgtv_Transiciones.TabIndex = 11;
             // 
             // label7
             // 
@@ -298,13 +267,43 @@
             this.label7.TabIndex = 10;
             this.label7.Text = "Tabla de transiciones:";
             // 
-            // dgtv_Transiciones
+            // dgv_Follows
             // 
-            this.dgtv_Transiciones.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgtv_Transiciones.Location = new System.Drawing.Point(19, 248);
-            this.dgtv_Transiciones.Name = "dgtv_Transiciones";
-            this.dgtv_Transiciones.Size = new System.Drawing.Size(556, 150);
-            this.dgtv_Transiciones.TabIndex = 11;
+            this.dgv_Follows.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_Follows.Location = new System.Drawing.Point(19, 58);
+            this.dgv_Follows.Name = "dgv_Follows";
+            this.dgv_Follows.RowHeadersVisible = false;
+            this.dgv_Follows.Size = new System.Drawing.Size(219, 150);
+            this.dgv_Follows.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(15, 35);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(124, 20);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Tabla de Follows:";
+            // 
+            // btn_GenerarArchivo
+            // 
+            this.btn_GenerarArchivo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_GenerarArchivo.Image = global::ProyectoFinal_RicardoChian.Properties.Resources.ContinuarVerde;
+            this.btn_GenerarArchivo.Location = new System.Drawing.Point(587, 371);
+            this.btn_GenerarArchivo.Name = "btn_GenerarArchivo";
+            this.btn_GenerarArchivo.Size = new System.Drawing.Size(70, 37);
+            this.btn_GenerarArchivo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btn_GenerarArchivo.TabIndex = 7;
+            this.btn_GenerarArchivo.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(66, 354);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 17);
+            this.label5.TabIndex = 6;
             // 
             // Home
             // 
@@ -329,14 +328,14 @@
             ((System.ComponentModel.ISupportInitialize)(this.btn_CargarArchivo)).EndInit();
             this.gbx_ResultadoAnalisis.ResumeLayout(false);
             this.gbx_ResultadoAnalisis.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dgv_Sets)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dgtv_Tokens)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.btn_continuarAutomata)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgtv_Tokens)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Sets)).EndInit();
             this.gbx_Automata.ResumeLayout(false);
             this.gbx_Automata.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.btn_GenerarArchivo)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dgv_Follows)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgtv_Transiciones)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Follows)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_GenerarArchivo)).EndInit();
             this.ResumeLayout(false);
 
         }
